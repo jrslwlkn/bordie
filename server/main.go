69c3ecd5ad5726b7eb54f92a171287a055fb3a3e
Yareeque <jrslwlkn@gmail.com>
2022-06-21 00:04:49 -0500

    server works with live database

--- server/main.go
+++ server/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
 )
 
@@ -14,12 +17,21 @@ var session *r.Session
 const PAGE_LEN = 20
 
 func main() {
-	router := gin.Default()
-
 	var err error
+
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file.")
+	}
+
+	router := gin.Default()
+	router.Use(CORSMiddleware()) // FIXME: this middleware is only for dev env
+
 	session, err = r.Connect(r.ConnectOpts{
-		Address:  "localhost:28015",
-		Database: "bordie",
+		Address:  os.Getenv("RETHINKDB_HOST") + ":" + os.Getenv("RETHINKDB_PORT"),
+		Database: os.Getenv("RETHINKDB_NAME"),
+		Username: os.Getenv("RETHINKDB_USERNAME"),
+		Password: os.Getenv("RETHINKDB_PASSWORD"),
 	})
 	if err != nil {
 		log.Fatalln(err.Error())
@@ -32,6 +44,23 @@ func main() {
 	router.POST("/post", postPost)
 
 	router.Run("localhost:8088")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 func getBoards(c *gin.Context) {
@@ -49,7 +78,7 @@ func getBoards(c *gin.Context) {
 
 func getBoard(c *gin.Context) {
 	board := c.Params.ByName("boardId")
-	// page := c.Params.ByName("page")
+	// page := c.Params.ByName("page") // TODO
 
 	res, err := r.Table("threads").Filter(r.Row.Field("id").Eq(board)).Run(session)
 	if err != nil {
@@ -72,4 +101,12 @@ func getPost(c *gin.Context) {
 }
 
 func postPost(c *gin.Context) {
+	data, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	jsonString := string(data[:])
+	// TODO
+	fmt.Println("data: ...")
+	fmt.Println(jsonString)
 }
